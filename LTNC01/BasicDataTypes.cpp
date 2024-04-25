659ccd28de2a508daf318cd8e5289b663e8d975e
ducandlinh <165494666+ducandlinh@users.noreply.github.com>
2024-04-25 09:54:38 +0700

    Create BasicDataTypes.cpp

---
+++ LTNC01/BasicDataTypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int n ;
+    long s ;
+    char d ;
+    float t ;
+    double h ;
+    cin >> n >> s >> d >> t >> h;
+    cout << n << endl;
+    cout << s << endl;
+    cout << d << endl;
+    cout << fixed << setprecision(10) << t << endl;
+    cout << fixed << setprecision(10) << h;
+    return 0;
+}
