8a650a1ad00f7e0ee6fd18dfceacd69b94981308
ducandlinh <165494666+ducandlinh@users.noreply.github.com>
2024-04-25 10:06:29 +0700

    Create camelcase.cpp

---
+++ LTNC03/camelcase.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int camelcase(string s) {
+int x;
+int y=1;
+for(char c : s){x = (int) c; if(x >= 65 && x <= 90){y = y + 1;}}
+return y;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = camelcase(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
