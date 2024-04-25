af4456a9eaebf43089c4fb55ec5954ee7cf720f6
ducandlinh <165494666+ducandlinh@users.noreply.github.com>
2024-04-25 09:53:45 +0700

    Create inputandoutput.cpp

---
+++ LTNC01/inputandoutput.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int t;
+    int a, b, c;
+    cin >> a >> b >> c;
+    t = a+b+c;
+    cout << t;
+    return 0;
+}
