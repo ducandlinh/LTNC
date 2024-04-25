5d5e8f5beacf442f4d9b81f707e06670727e1c2a
ducandlinh <165494666+ducandlinh@users.noreply.github.com>
2024-04-25 10:20:25 +0700

    Create variablesizedthearray.cpp

---
+++ LTNC04/variablesizedthearray.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n, q, k;
+    cin>>n>>q;
+    vector<vector<int>>a(n);
+    for(int i=0;i<n;i++){
+        cin>>k;
+        a[i].resize(k);
+        for(int j=0;j<k;j++){
+            cin>>a[i][j];
+        }
+    }
+    int row,col;
+    while(q--){
+        cin>>row>>col;
+        cout<<a[row][col]<<endl;
+    }
+}
